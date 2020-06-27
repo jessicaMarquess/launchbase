d27a34bb04fa2136e5bd11d61f454b222bda317f
Jessica Marques <jessica.marques.dev@gmail.com>
2020-06-27 11:51:49 -0300

    modulo + desafio 3-1

---
+++ modulo-3/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server
+});
+server.get("/", function (req, res) {
+    return res.render("about");
+});
+server.get("/content", function (req, res) {
+    return res.render("content");
+});
+server.use(function (req, res) {
+    res.status(404).render("not-found");
+});
+server.listen(5000, function () {
+    console.log("Server is running");
+});
