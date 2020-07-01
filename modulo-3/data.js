f92272047eb01ab7920cd9fd035b674de09f5c37
Jessica Marques <jessica.marques.dev@gmail.com>
2020-07-01 17:25:17 -0300

    refactoring

---
+++ modulo-3/data.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: "starter",
+        title: "Starter",
+        aboutStrong: "Torne-se um programador desejado",
+        aboutSimple: "com esses cursos gratuitos no mercado",
+        modules: "5 Módulos",
+        price: "Free"
+    },
+    {
+        id: "launchbase",
+        title: "LaunchBase",
+        aboutStrong: "Domine programação do zero",
+        aboutSimple: "e tenha acesso às melhores oportunidades do mercado",
+        modules: "12 Módulos",
+        price: "BUY"
+    },
+    {
+        id: "gostack",
+        title: "GoStack",
+        aboutStrong: "Treino Imersivo",
+        aboutSimple: "nas tecnologias mais modernas de desenvolvimento web e mobile",
+        modules: "15 Módulos",
+        price: "BUY"
+    },
+]
--- modulo-3/server.js
+++ modulo-3/server.js
@@ -2,19 +2,46 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const courses = require ('./data');
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk");
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
 });
 server.get("/", function (req, res) {
-    return res.render("about");
+    const about = {
+        title: "Rocketseat",
+        description: "As melhores tecnologias em programação, direto ao ponto e do jeito certo. No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa.",
+        skills: [
+            { name: "NodeJS", url: "https://nodejs.org/en/"},
+            { name: "ReactJS", url: "https://pt-br.reactjs.org/"},
+            { name: "React Native", url: "https://reactnative.dev/"}
+        ]
+    }
+    return res.render("about", {about});
 });
-server.get("/content", function (req, res) {
-    return res.render("content");
+server.get("/courses", function (req, res) {
+    return res.render("courses", {items: courses});
+});
+
+server.get("/course/:id", function(req, res) {
+    const id = req.params.id;
+
+    const course = courses.find(function(course){
+        return course.id == id;
+    })
+
+    if(!course){
+         return res.status(404).render("not-found");
+    }
+    
+    return res.render ("course", {item: course});
+
 });
 server.use(function (req, res) {
     res.status(404).render("not-found");
