ab18f6df8bbf0fa9a4b1d9b8a86d94ee0f7aba5b
Jessica Marques <jessica.marques.dev@gmail.com>
2020-07-12 17:50:33 -0300

    gym+seateachers+update

---
+++ modulo-4/Desafio 4-1/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = express.Router();
+
+routes.get("/", function(req, res){
+    res.redirect("teachers");
+});
+
+routes.get("/teachers", function(req, res){
+    res.render('teachers');
+});
+
+routes.get("/students", function(req, res){
+    res.render("students");
+});
+
+
+
+module.exports = routes;
---
+++ modulo-4/Gym/public/scripts.js
@@ -0,0 +1,11 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards){
+    card.addEventListener("click", function(){
+        const courseId = card.getAttribute("id");
+        window.location.href = `/course/${courseId}`;
+    })
+};
+
+
