eeeb45ea83463eed9ba67a159d3308724fc7ae91
Jessica Marques <jessica.marques.dev@gmail.com>
2020-07-14 18:38:13 -0300

    commit att create.njk

--- modulo-4/Gym/instructors.js
+++ modulo-4/Gym/instructors.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const data = require('./data.json');
-const { Z_DATA_ERROR } = require('zlib');
 
 //create
 exports.post = function(req, res){
+
     const keys = Object.keys(req.body);
 
     for(key in keys){
@@ -12,10 +12,24 @@ exports.post = function(req, res){
         return res.send("Please, fill all fields");
     }
 
-    req.body.birth = Date.parse(req.body.birth);
-    req.body.created_at = Date.now();
+    let {avatar_url, birth, name, services, gender} = req.body;
 
-    data.instructors.push(req.body);
+
+    birth = Date.parse(birth);
+    const created_at = Date.now();
+    const id = Number(data.instructors.length + 1);
+
+ 
+
+    data.instructors.push({
+        avatar_url,
+        birth,
+        created_at,
+        id,
+        gender,
+        services,
+        name
+    });
 
     fs.writeFile("data.json", JSON.stringify(data, null, 4), function(err){
         if(err) return res.send("Write file error");
