d5caab455637dd04bc334c7bb87cb289afc97ef8
Jessica Marques <jessica.marques.dev@gmail.com>
2020-07-17 17:23:24 -0300

    update gym

--- modulo-4/Gym/instructors.js
+++ modulo-4/Gym/instructors.js
@@ -76,6 +76,48 @@ exports.edit = function(req,res){
 }
 
 //update
+exports.put = function(req,res){
+    const {id} = req.body;
+    let index = 0;
+    
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+        if(id == instructor.id){
+            index = foundIndex;
+            return true;
+        }
+           
+    });
 
+    if(!foundInstructor) return res.send("Instructors not found!");
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor;
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 4), function(err){
+        if (err) return res.send('Write error!');
+
+        return res.redirect(`/instructors/${id}`);
+    });
+}
+exports.delete = function(req,res){
+    const {id} = req.body;
+
+    const filteredInstructors = data.instructors.filter(function(instructor) {
+        return instructor.id != id
+    });
+
+    data.instructors = filteredInstructors;
+    
+    fs.writeFile("data.json", JSON.stringify(data, null, 4), function (err){
+        if (err) return res.send("Write error!");
+
+        return res.redirect("/instructors");
+    });
+}
 
 //delete
