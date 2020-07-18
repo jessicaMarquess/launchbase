93f36f5d53b95e7d5859ca31604de59a02e41f95
Jessica Marques <jessica.marques.dev@gmail.com>
2020-07-18 20:00:12 -0300

    modification index

--- modulo-4/Gym/instructors.js
+++ modulo-4/Gym/instructors.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const data = require('./data.json');
 const { age, date } = require('./utils')
+
+//routes
+exports.index = function(req,res){
+    return res.render("instructors/index", {instructors: data.instructors});
+};
+
 // show
 exports.show = function(req,res){
     const {id} = req.params;
@@ -93,7 +99,8 @@ exports.put = function(req,res){
     const instructor = {
         ...foundInstructor,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
     }
 
     data.instructors[index] = instructor;
