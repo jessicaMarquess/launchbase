fb10b67673b23198612f420da1af753fe3fb2e78
Jessica Marques <jessica.marques.dev@gmail.com>
2020-07-19 12:04:22 -0300

    index modification

--- modulo-4/Desafio 4/teachers.js
+++ modulo-4/Desafio 4/teachers.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const data = require('./data.json');
 const { graduation, age, date } = require('./utils');
 
+exports.index =  function(req, res){
+    return res.render('teachers/index', {teachers: data.teachers});
+}
 exports.show = function(req,res){
     const {id} = req.params;
 
