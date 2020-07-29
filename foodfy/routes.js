7585582333883101dde5da3bb1845e3186076979
Jessica Marques <jessica.marques.dev@gmail.com>
2020-07-29 20:18:16 -0300

    commit routes

--- foodfy/routes.js
+++ foodfy/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 const recipes = require("./data");
+const recipe = require("./controllers/recipe");
 
 routes.get("/", function(req, res){
     return res.render("index", {items: recipes});
@@ -19,8 +20,14 @@ routes.get('/recipe/:index', function (req, res) {
     }
     return res.render('recipe', { item })
 });
+routes.get("./admin/recipe", recipe.index);
+// routes.get("/admin/recipes/create", recipes.create);
+// routes.get("/admin/recipes/:index", recipes.show);
+// routes.get("/admin/recipes/:id/edit", recipes.edit);
+// routes.post("/admin/recipes", recipes.post);
+// routes.put("/admin/recipes", recipes.put);
+// routes.delete("/admin/recipes", recipes.delete);
 routes.use(function (req, res) {
     res.status(404).render("not-found");
 });
-
 module.exports = routes;
