3d3d064dd4af62300204b932cb698edd5a32f7fe
Jessica Marques <jessica.marques.dev@gmail.com>
2020-08-11 10:30:43 -0300

    criando models instructors

--- modulo-5/gym/src/app/controllers/instructors.js
+++ modulo-5/gym/src/app/controllers/instructors.js
@@ -1,10 +1,13 @@
-
-const { age, date } = require('../../lib/utils');
+const {age, date} = require('../../lib/utils');
+const Instructor = require('../../app/models/Instructors');
 const Intl = require('intl');
 
 module.exports = {
     index(req, res){
-        return res.render('instructors/index');
+        Instructor.all(function(instructors){
+            return res.render('instructors/index', {instructors});
+        })
+        
     },
     create(req, res){
         return res.render("instructors/create");
@@ -16,14 +19,32 @@ module.exports = {
             if(req.body[key] == "")
             return res.send("Please, fill all fields");
         }
-        let {avatar_url, birth, name, services, gender} = req.body;
-        return;
+        Instructor.create(req.body, function(instructor){
+            return res.redirect (`/instructors/${instructor.id}`);
+        })
     },
     show(req, res){
-        return;
+        Instructor.find(req.params.id, function(instructor){
+            if(!instructor) return res.send("Instructor not exist, please try again!");
+
+            instructor.age = age(instructor.birth);
+            instructor.services = instructor.services.split(",");
+
+            instructor.created_at = date(instructor.created_at).format;
+
+
+            return res.render("instructors/show", {instructor});
+        });
     },
     edit(req, res){
-        return;
+
+        Instructor.find(req.params.id, function(instructor){
+            if(!instructor) return res.send("Instructor not exist, please try again!");
+
+            instructor.birth = date(instructor.birth).iso;
+
+            return res.render("instructors/edit", {instructor});
+        });
     },
     put(req, res){
         const keys = Object.keys(req.body);
@@ -32,10 +53,14 @@ module.exports = {
             if(req.body[key] == "")
             return res.send("Please, fill all fields");
         }
-        return;
+        Instructor.update(req.body, function(){
+            return res.redirect(`instructors/${req.body.id}`)
+        });
     },
     delete(req, res){
-        return;
+        Instructor.delete(req.body.id, function(){
+            return res.redirect(`instructors`);
+        });
     },
 }
 
