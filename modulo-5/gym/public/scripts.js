c531730157727614d405df0b6cefbed9375f02e4
Jessica Marques <jessica.marques.dev@gmail.com>
2020-08-17 15:31:04 -0300

    logica na paginacao

--- modulo-5/gym/public/scripts.js
+++ modulo-5/gym/public/scripts.js
@@ -6,3 +6,34 @@ for(item of menuItems){
         item.classList.add("active");
     }
 }
+
+//paginacao
+//totalPages = 20
+//selectedPage = 15
+// [ 1, ..., 13, 14, 15, 16, 17, ..., 20]
+
+let totalPages = 20,
+    selectedPage = 6,
+    pages = [],
+    oldPage;
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++){
+
+    const firstAndLastPages = currentPage == 1 || currentPage == totalPages;
+    const pageAfterSelectedPages = currentPage <= selectedPage + 2; 
+    const pageBeforeSelectedPages =  currentPage >= selectedPage - 2; 
+    
+    if( firstAndLastPages || pageBeforeSelectedPages && pageAfterSelectedPages){
+        if(oldPage && currentPage - oldPage > 2){
+            pages.push("...");
+        }
+        if(oldPage && currentPage - oldPage == 2){
+            pages.push(currentPage - 1);
+        }
+
+        pages.push(currentPage);
+        oldPage = currentPage;
+    }
+}
+
+console.log(pages);
