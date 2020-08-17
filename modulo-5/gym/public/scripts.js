4d5e3a15c87ea514113370333c1703d4d1369a17
Jessica Marques <jessica.marques.dev@gmail.com>
2020-08-17 16:35:44 -0300

    finalizacao

--- modulo-5/gym/public/scripts.js
+++ modulo-5/gym/public/scripts.js
@@ -5,35 +5,59 @@ for(item of menuItems){
     if (currentPage.includes(item.getAttribute('href'))){
         item.classList.add("active");
     }
-}
-
-//paginacao
-//totalPages = 20
-//selectedPage = 15
-// [ 1, ..., 13, 14, 15, 16, 17, ..., 20]
-
-let totalPages = 20,
-    selectedPage = 6,
-    pages = [],
+};
+function paginate(selectedPage, totalPages){
+    
+let pages = [],
     oldPage;
 
 for (let currentPage = 1; currentPage <= totalPages; currentPage++){
 
-    const firstAndLastPages = currentPage == 1 || currentPage == totalPages;
-    const pageAfterSelectedPages = currentPage <= selectedPage + 2; 
-    const pageBeforeSelectedPages =  currentPage >= selectedPage - 2; 
-    
-    if( firstAndLastPages || pageBeforeSelectedPages && pageAfterSelectedPages){
-        if(oldPage && currentPage - oldPage > 2){
-            pages.push("...");
-        }
-        if(oldPage && currentPage - oldPage == 2){
-            pages.push(currentPage - 1);
-        }
+const firstAndLastPages = currentPage == 1 || currentPage == totalPages;
+const pageAfterSelectedPages = currentPage <= selectedPage + 2; 
+const pageBeforeSelectedPages =  currentPage >= selectedPage - 2; 
 
-        pages.push(currentPage);
-        oldPage = currentPage;
+if( firstAndLastPages || pageBeforeSelectedPages && pageAfterSelectedPages){
+    if(oldPage && currentPage - oldPage > 2){
+        pages.push("...");
     }
+    if(oldPage && currentPage - oldPage == 2){
+        pages.push(currentPage - 1);
+    }
+
+    pages.push(currentPage);
+    oldPage = currentPage;
+}
 }
 
-console.log(pages);
+return pages;
+
+};
+function createPagination(pagination){
+    const filter = pagination.dataset.filter;
+    const page = +pagination.dataset.page;
+    const total = +pagination.dataset.total;
+    const pages = paginate(page, total);
+    
+    let elements = "";
+    
+    for (let page of pages){
+        if (String(page).includes("...")) {
+            elements += `<span>${page}</span>`
+        } else {
+            if (filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            } else {
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+        }
+    }
+    
+    pagination.innerHTML = elements;
+};
+const pagination = document.querySelector(".pagination");
+
+if(paginate){
+    createPagination(pagination);
+};
+
