69b7b74c9304bb509bc7932818507a8a6255b320
Jessica Marques <jessica.marques.dev@gmail.com>
2020-08-22 10:10:36 -0300

    pagina de edicao e opcao de delete

--- modulo-6/src/routes.js
+++ modulo-6/src/routes.js
@@ -6,7 +6,11 @@ routes.get('/', function(req,res){
     return res.render("layout.njk");
 });
 routes.get('/products/create', ProductController.create);
-routes.post('/products', ProductController.post)
+routes.get('/products/:id/edit', ProductController.edit);
+
+routes.post('/products', ProductController.post);
+routes.put('/products', ProductController.put);
+routes.delete('/products', ProductController.delete);
 
 routes.get('/ads/create', function(req,res){
     return res.redirect('/products/create');
