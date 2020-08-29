3eb6f60413523fbc462fedf199729ac0c185c04f
Jessica Marques <jessica.marques.dev@gmail.com>
2020-08-29 13:13:57 -0300

    modificacao File.js

--- modulo-6/src/app/models/File.js
+++ modulo-6/src/app/models/File.js
@@ -24,14 +24,11 @@ module.exports = {
             const result = await db.query(`SELECT * FROM files WHERE id = $1`, [id]);
             const file = result.rows[0];
     
-            fs.unlink(file.path, (err)=>{
-                if(err) throw err
-                
-                return db.query(`
-                DELETE FROM files WHERE id = $1
-            `, [id]);
-            });
-
+            fs.unlinkSync(file.path)
+            
+            return db.query(`
+            DELETE FROM files WHERE id = $1
+        `, [id]);
         }catch(err){
             console.error(err);
         }
