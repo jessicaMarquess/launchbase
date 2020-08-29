a30eb11e3ace01541c23795ae1ecb2794f060886
Jessica Marques <jessica.marques.dev@gmail.com>
2020-08-29 12:14:28 -0300

    backend edit

--- modulo-6/src/app/controllers/ProductController.js
+++ modulo-6/src/app/controllers/ProductController.js
@@ -53,7 +53,14 @@ module.exports = {
         results = await Category.all();
         const categories = results.rows;
 
-        return res.render('products/edit.njk', { product, categories });
+        results = await Product.files(product.id);
+        let files = results.rows;
+        files = files.map(file => ({ 
+            ...file,
+            src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+        }))
+
+        return res.render('products/edit.njk', { product, categories, files });
     },
     async put(req, res){
         const keys = Object.keys(req.body);
