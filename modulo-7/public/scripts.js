db234fc5736cc1c840157b07d01aa3cbee62db34
Jessica Marques <jessica.marques.dev@gmail.com>
2020-09-10 09:32:28 -0300

    validacao dos dados users no front

--- modulo-7/public/scripts.js
+++ modulo-7/public/scripts.js
@@ -11,6 +11,43 @@ const Mask = {
             style: 'currency',
             currency: 'BRL'
         }).format(value/100);
+    },
+    cpfCnpj(value) {
+        value = value.replace(/\D/g,"");
+
+        if(value.length > 14){
+            value = value.slice(0, -1);
+        }
+        //check if  cnpj: - 11.222.333/0001-11
+        if(value.length > 11){ //sera um cnpj
+            value = value.replace(/(\d{2})(\d)/,"$1.$2");
+             // 11.2223330001-11
+            value = value.replace(/(\d{3})(\d)/,"$1.$2");
+             // 11.222.3330001-11
+            value = value.replace(/(\d{3})(\d)/,"$1/$2");
+             // 11.222.333/000111
+            value = value.replace(/(\d{4})(\d)/,"$1-$2");
+             // 11.222.333/0001-11
+        } else {    //sera um cpf
+            //cpf 111.222.333-55
+            value = value.replace(/(\d{3})(\d)/,"$1.$2");
+            // 111.22233355
+            value = value.replace(/(\d{3})(\d)/,"$1.$2");
+            // 111.222.33355
+            value = value.replace(/(\d{3})(\d)/,"$1-$2");
+            //111.222.333-55
+        };
+        return value;
+
+    },
+    cep(value) {
+        value = value.replace(/\D/g,"");
+        if(value.length > 8){
+            value = value.slice(0, -1);
+        }
+        value = value.replace(/(\d{5})(\d)/,"$1-$2");
+
+        return value;
     }
 };
 const PhotosUpload = {
@@ -143,5 +180,69 @@ const Lightbox = {
         Lightbox.target.style.top = "-100%";
         Lightbox.target.style.bottom = "initial";
         Lightbox.closeButton.style.top = "-80px";
+    }
+};
+const Validate = {
+    apply(input, func) {
+            Validate.clearErrors(input)
+
+            let results = Validate[func](input.value);
+            input.value = results.value;
+
+            if(results.error)
+                Validate.displayError(input, results.error);
+    
+    },
+    clearErrors(input){
+        const errorDiv = input.parentNode.querySelector(".error")
+        if(errorDiv)
+            errorDiv.remove();
+    },
+    displayError(input, error){
+        const div = document.createElement('div');
+        div.classList.add('error');
+        div.innerHTML = error;
+        input.parentNode.appendChild(div);
+        input.focus();
+    },
+    isEmail(value) {
+        let error = null;
+        const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if(!value.match(mailFormat))
+            error = "Email Inválido";
+
+        return{
+            error, 
+            value
+        };
+    },
+    isCpfCnpj(value){
+        let error = null;
+        const cleanValues = value.replace(/\D/g,"");
+        
+        if(cleanValues.length > 11 && cleanValues.length !== 14){
+            error = "CNPJ incorreto";
+        } else if (cleanValues.length < 12 && cleanValues.length !== 11){
+            error = "CPF incorreto";
+        };
+        
+        return{
+            error, 
+            value
+        };
+    },
+    isCep(value){
+        let error = null;
+        
+        const cleanValues = value.replace(/\D/g,"");
+        if(cleanValues.length !== 8){
+            error = "CEP incorreto";
+        }
+        return{
+            error, 
+            value
+        };
+
     }
 };
