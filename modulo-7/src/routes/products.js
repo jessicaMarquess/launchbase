ed48e206cedb87e0acf9c42725eed51fe9f0dff0
Jessica Marques <jessica.marques.dev@gmail.com>
2020-09-17 16:28:34 -0300

    creation middleware from verification users

--- modulo-7/src/routes/products.js
+++ modulo-7/src/routes/products.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const routes = express.Router();
 const multer = require('../app/middlewares/multer');
-const SearchController = require('../app/controllers/SearchController');
+
 const ProductController = require('../app/controllers/ProductController');
+const SearchController = require('../app/controllers/SearchController');
+
+const { onlyUsers } = require('../app/middlewares/session');
 
 //search
 routes.get('/search', SearchController.index);
 
 //products
-routes.get('/create', ProductController.create);
+routes.get('/create', onlyUsers, ProductController.create);
 routes.get('/:id', ProductController.show);
-routes.get('/:id/edit', ProductController.edit);
+routes.get('/:id/edit', onlyUsers, ProductController.edit);
 
-routes.post('/', multer.array("photos", 6), ProductController.post);
-routes.put('/', multer.array("photos", 6), ProductController.put);
-routes.delete('/', ProductController.delete);
+routes.post('/', onlyUsers, multer.array("photos", 6), ProductController.post);
+routes.put('/', onlyUsers, multer.array("photos", 6), ProductController.put);
+routes.delete('/', onlyUsers, ProductController.delete);
 
 module.exports = routes;
