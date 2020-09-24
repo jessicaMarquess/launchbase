786cf7a48b5dc09fd53d53658e030d9e32f3af8c
Jessica Marques <jessica.marques.dev@gmail.com>
2020-09-24 17:28:47 -0300

    refactoring filters and searchController

--- modulo-8/src/app/controllers/SearchController.js
+++ modulo-8/src/app/controllers/SearchController.js
@@ -5,25 +5,19 @@ module.exports = {
     async index(req, res) {
 
         try {
-            let params = {};
 
-            const { filter, category } = req.query;
+            let { filter, category } = req.query;
             
-            if(!filter) return res.redirect("/");
-            
-            params.filter = filter;
+            if(!filter || filter.toLowerCase() == 'toda a loja') filter = null;
 
-            if(category){
-                params.category = catergory;
-            }
-            let products = await Product.search(params);
+            let products = await Product.search({filter, category});
             
             const productsPromise = products.map(LoadProductService.format);
 
             products = await Promise.all(productsPromise);
 
             const search = {
-                term: req.query.filter,
+                term: filter || 'Toda a loja',
                 total: products.length
             }
 
--- modulo-8/src/app/models/Product.js
+++ modulo-8/src/app/models/Product.js
@@ -1,4 +1,5 @@
 const db = require('../../config/db');
+const { product } = require('../services/LoadProductService');
 const Base = require('./Base');
 
 Base.init({table: 'products'});
@@ -12,31 +13,28 @@ module.exports= {
 
         return results.rows;
     },
-    async search(params){
-        const {filter, category} = params;
+    async search({filter, category}){
 
-        let query = "",
-            filterQuery = `WHERE`;
-        
-        if(category){
-            filterQuery = ` ${filterQuery}
-            products.category_id = ${category}
-            AND`
-        }
-
-        filterQuery = `
-            ${filterQuery}
-            products.name ilike '${filter}'
-            OR products.description ilike '${filter}'
-        `
-
-        query = `
+        let query = `
             SELECT products.*,
                 categories.name AS category_name
             FROM products 
             LEFT JOIN categories ON (categories.id = products.category_id)
-            ${filterQuery}
-        `
+            WHERE 1 = 1
+        `;
+        
+        if(category){
+            query += `AND products.category_id = ${category}`;
+        };
+
+        if(filter){
+            query += `
+            AND (products.name ilike '%${filter}%'
+            OR products.description ilike '%${filter}%')`
+        };
+
+        query += ` AND status != 0`;
+
         const results = await db.query(query);
         return results.rows;
     },
