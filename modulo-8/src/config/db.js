21f41392ae7f554de0cb478139faa7c0f0bc20c4
Jessica Marques <jessica.marques.dev@gmail.com>
2020-10-21 09:11:08 -0300

    add card.js

--- modulo-8/src/config/db.js
+++ modulo-8/src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
     user: 'postgres',
-    password: "",
+    password: "3881314ccfc84fc5ab0d99a55eacacd5",
     host: "localhost",
     port: 5432,
     database: "launchstoredb"
