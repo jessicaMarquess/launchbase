780256b8657d64403d039425889b702ba18a6065
Jessica Marques <jessica.marques.dev@gmail.com>
2020-10-21 09:42:45 -0300

    method of add product done

--- modulo-8/src/lib/card.js
+++ modulo-8/src/lib/card.js
@@ -16,11 +16,44 @@ const Cart = {
         return this;
     },
     addOne(product){
+        let inCart = this.items.find(item => item.product.id == product.id);
+
+        if(!inCart){
+            inCart = {
+                product:{
+                    ...product,
+                    formattedPrice: formatPrice(product.price);
+                },
+                quantity: 0,
+                price: 0,
+                formattedPrice: formatPrice(0),
+            }
+            this.items.push(inCart);
+        };
+
+        if(inCart.quantity>= product.quantity) return this;
+
+        inCart.quantity++;
+        inCart.price = inCart.product.price * inCart.quantity;
+        inCart.formattedPrice = formatPrice(inCart.price);
+
+        this.total.quantity++;
+        this.total.price += inCart.product.price;
+        this.total.formattedPrice = formatPrice(this.total.price);
+
+        return this;
     },
     removeOne(productId){
     },
     delete(productId){
     },
 };
+const product = {
+    id: 1,
+    price: 199,
+    quantity: 2,
+};
+
+console.log(Card.init().addOne(product));
 
 module.exports = Cart;
